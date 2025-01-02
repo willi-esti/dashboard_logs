560a1b5db56edfd92fccbeb6f69d5a17302baada
ca.it-t <ca@it-t.fr>
2025-01-02 12:24:40 +0000

    feat: update systemctl script to use the correct path for improved service management

--- system/systemctl.sh
+++ system/systemctl.sh
@@ -2,9 +2,9 @@
 
 if [[ "$1" =~ ^(start|stop|restart|status)$ ]] && [[ "$2" =~ ^[a-zA-Z0-9\._-]+$ ]]; then
     if [[ "$1" == "status" ]]; then
-        /bin/systemctl "$1" "$2"
+        /usr/bin/systemctl "$1" "$2"
     else
-        sudo /bin/systemctl "$1" "$2"
+        sudo /usr/bin/systemctl "$1" "$2"
     fi
 else
     echo "Usage: $0 {start|status|stop|restart} service_name"
