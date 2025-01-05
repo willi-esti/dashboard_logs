470eb8d5ca51c6b8479cc4bb0aeecf5e31ac5a99
ca.it-t <ca@it-t.fr>
2025-01-05 16:12:31 +0000

    feat: remove unnecessary SELinux context management for systemctl in installation script

--- install.sh
+++ install.sh
@@ -672,11 +672,6 @@ if [ "$ADD_SUDO_RULES" = true ]; then
         RESTART_RULE="${WEB_USER} ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart $service"
         STOP_RULE="${WEB_USER} ALL=(ALL) NOPASSWD: /usr/bin/systemctl stop $service"
 
-        install_packages policycoreutils policycoreutils-python-utils
-        
-        semanage fcontext -a -t httpd_sys_script_exec_t /usr/bin/systemctl
-        restorecon -v /usr/bin/systemctl
-
         if sudo grep -Fxq "$RESTART_RULE" /etc/sudoers.d/${WEB_USER}-restart; then
             info "Restart rule for $service already exists in sudoers file."
         else
